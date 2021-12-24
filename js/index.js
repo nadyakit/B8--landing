3101fcfc44c5071d78ef6317a55d1f61f2e86bc5
Aleksandr Kazakov <emtyxd@mail.ru>
2021-12-25 01:58:58 +0300

    cards

--- js/index.js
+++ js/index.js
@@ -1,17 +1,22 @@
 //Init swiper
 const swiper = new Swiper('.swiper', {
+  autoplay: true,
+  autoHeight: true,
   slidesPerView: 3,
-  spaceBetween: 50,
+  spaceBetween: 20,
   direction: 'horizontal',
   loop: true,
   pagination: {
     el: '.swiper-pagination',
   },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
   scrollbar: {
     el: '.swiper-scrollbar',
   },
+})
+// swiper arrows
+$( window ).resize(function() {
+
+})
+$( document ).ready(function() {
+
 })
