e69c5d5c95cd564da1a4e22b75b8c29690b35708
Aleksandr Kazakov <emtyxd@mail.ru>
2021-12-25 09:13:49 +0300

    mini burger

--- js/index.js
+++ js/index.js
@@ -24,3 +24,36 @@ const swiper_mobile = new Swiper('.swiper-mobile', {
   direction: 'horizontal',
   loop: true,
 })
+
+let isOpen = false;
+function closeBurger(){
+  $(".text_first").css("opacity", "1")
+  $(".button_first").css("opacity", "1")
+
+  $(".menu-items").css("opacity", "0")
+
+  $("#burger-btn img").attr("src","img/burger.svg")
+  $("#burger-btn img").css("height", "5vw")
+  $("#burger-btn img").css("width", "10vw")
+}
+function openBurger(){
+  $(".text_first").css("opacity", "0")
+  $(".button_first").css("opacity", "0")
+
+  $(".menu-items").css("opacity", "1")
+
+  $("#burger-btn img").attr("src","img/close.svg")
+  $("#burger-btn img").css("height", "7vw")
+  $("#burger-btn img").css("width", "7vw")
+}
+$("#burger-btn").click(function() {
+  if (isOpen) {
+    closeBurger()
+    isOpen = false
+  }
+  else {
+    openBurger()
+    isOpen = true
+  }
+  console.log(isOpen)
+})
